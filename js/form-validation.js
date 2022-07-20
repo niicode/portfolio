8cee63640276122fb3545ee82cf5308f7255da4c
niicode <niidarku66@gmail.com>
2022-07-20 12:46:44 +0000

    update form-validation

--- js/form-validation.js
+++ js/form-validation.js
@@ -2,8 +2,7 @@ document.getElementById('contact').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
   if (email.value !== email.toLowerCase()) {
     event.preventDefault();
-    document.querySelector('.error').innerHTML =
-      'Your email address should be in lowercase';
+    document.querySelector('.error').innerHTML = 'Your email address should be in lowercase';
     event.target.reset();
   }
 });

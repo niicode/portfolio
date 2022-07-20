d6d2efe133803098c6d122600a8675d14dd32f59
niicode <niidarku66@gmail.com>
2022-07-20 13:23:08 +0000

    fix email validation

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,6 +1,6 @@
 document.getElementById('contact').addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
-  if (email.value !== email.toLowerCase()) {
+  if (email !== email.toLowerCase()) {
     event.preventDefault();
     document.querySelector('.error').innerHTML = 'Your email address should be in lowercase';
     event.target.reset();

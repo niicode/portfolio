23f36af0afe1f477b41ed9843360256476d33794
niicode <niidarku66@gmail.com>
2022-07-21 09:36:42 +0000

    add local storage

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,13 +1,31 @@
-function storeFormField(e) {
-  /* eslint-disable */
-  const field = e.target.dataset.field;
-  /* eslint-enable  */
-  // get the form fields
-  const fieldValue = document.getElementById(field).value;
-  // unserialize form object from local storage
-  const formFields = JSON.parse(localStorage.getItem('formObject'));
-  // update name field in formfields object
-  formFields[field] = fieldValue;
-  // serialize form object to local storage
-  localStorage.setItem('formObject', JSON.stringify(formFields));
+function storeFormData() { 
+  const formObject = JSON.parse(localStorage.getItem('formObject'));
+  formObject.name = document.querySelector('#name').value;
+  formObject.email = document.querySelector('#email').value;
+  formObject.message = document.querySelector('#message').value;
+  localStorage.setItem('formObject', JSON.stringify(formObject));
 }
+
+function checkLocalStorage() {
+  if (!localStorage.getItem('formObject')) {
+    const formObject = {
+      name: '',
+      email: '',
+      message: '',
+    };
+    localStorage.setItem('formObject', JSON.stringify(formObject));
+  } else {
+    const formObject = JSON.parse(localStorage.getItem('formObject'));
+    document.querySelector('#name').value = formObject.name;
+    document.querySelector('#email').value = formObject.email;
+    document.querySelector('#message').value = formObject.message;
+  }
+}
+
+window.onload = () => {
+  checkLocalStorage(); // check if local storage is empty and if it is, create an empty object
+}
+
+document.querySelector('#name').addEventListener('keydown', storeFormData);
+document.querySelector('#email').addEventListener('keydown', storeFormData);
+document.querySelector('#message').addEventListener('keydown', storeFormData);
